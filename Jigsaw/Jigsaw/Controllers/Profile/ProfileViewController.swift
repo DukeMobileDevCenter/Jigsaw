dcd7c81d93d73dcd191b7d6c70e66fbca91b5c03
Ting Chen <tchen@esri.com>
2020-08-12 23:33:37 -0700

    Make UI changes.

--- Jigsaw/Jigsaw/Controllers/Profile/ProfileViewController.swift
+++ Jigsaw/Jigsaw/Controllers/Profile/ProfileViewController.swift
@@ -17,49 +17,50 @@ class ProfileViewController: FormViewController {
     }
     
     func createForm() {
-        form
-            +++ Section("Profile")
-            <<< ViewRow<ProfileHeaderView>("view")
-            .cellSetup { cell, _ in
-                //  Construct the view
-                let gravatarHash = Profiles.currentPlayer.userID.sha256
-                let endIndex = gravatarHash.index(gravatarHash.startIndex, offsetBy: 32)
-                let avatarURL = URL(string: "https://www.gravatar.com/avatar/\(gravatarHash[..<endIndex])?d=wavatar")!
-                print(avatarURL.absoluteString)
-                let view = Bundle.main.loadNibNamed("ProfileHeaderView", owner: self)?.first as! ProfileHeaderView
-                view.setView(name: Profiles.displayName, avatarURL: avatarURL)
-                cell.view = view
-            }
-            .onCellSelection { [weak self] _, _ in
+        form +++ Section("Profile")
+        <<< ViewRow<ProfileHeaderView>("view")
+        .cellSetup { cell, _ in
+            //  Construct the view
+            let gravatarHash = Profiles.currentPlayer.userID.sha256
+            let endIndex = gravatarHash.index(gravatarHash.startIndex, offsetBy: 32)
+            let avatarURL = URL(string: "https://www.gravatar.com/avatar/\(gravatarHash[..<endIndex])?d=wavatar")!
+            print(avatarURL.absoluteString)
+            let view = Bundle.main.loadNibNamed("ProfileHeaderView", owner: self)?.first as! ProfileHeaderView
+            view.setView(name: Profiles.displayName, avatarURL: avatarURL)
+            cell.view = view
+        }
+        .onCellSelection { [weak self] _, _ in
+            DispatchQueue.main.async {
                 self?.presentAlert(title: "More to add here", message: "Change avatar feature is on the way!")
             }
-            +++ Section(header: "Basic information", footer: "Blah blah blah")
-            <<< NameRow { row in
-                row.title = "Display name"
-                row.placeholder = "Change your nickname"
-                row.value = Profiles.displayName
-            }
-            <<< DecimalRow { row in
-                row.title = "Jigsaw value"
-                row.value = Profiles.jigsawValue
-                row.disabled = true
-            }
-            <<< DateRow { row in
-                row.title = "Join date"
-                row.value = Profiles.currentPlayer.joinDate
-                row.disabled = true
-            }
-            +++ Section("Demographics")
-            <<< ButtonRow { row in
-                row.title = "Update your demographics"
-                row.presentationMode = .show(controllerProvider: ControllerProvider.callback { return DemographicsViewController() }, onDismiss: { vc in vc.navigationController?.popViewController(animated: true) })
-            }
-            +++ Section("Game history")
-            <<< ButtonRow { row in
-                row.title = "Show game history"
-            }
-            .onCellSelection { [weak self] _, _ in
-                self?.presentAlert(title: "More to add here", message: "Show game history is on the way!")
-            }
+        }
+        +++ Section(header: "Basic information", footer: "Blah blah blah")
+        <<< NameRow { row in
+            row.title = "Display name"
+            row.placeholder = "Change your nickname"
+            row.value = Profiles.displayName
+        }
+        <<< DecimalRow { row in
+            row.title = "Jigsaw value"
+            row.value = Profiles.jigsawValue
+            row.disabled = true
+        }
+        <<< DateRow { row in
+            row.title = "Join date"
+            row.value = Profiles.currentPlayer.joinDate
+            row.disabled = true
+        }
+        +++ Section("Demographics")
+        <<< ButtonRow { row in
+            row.title = "Update your demographics"
+            row.presentationMode = .show(controllerProvider: ControllerProvider.callback { return DemographicsViewController() }, onDismiss: { vc in vc.navigationController?.popViewController(animated: true) })
+        }
+        +++ Section("Game history")
+        <<< ButtonRow { row in
+            row.title = "Show game history"
+        }
+        .onCellSelection { [weak self] _, _ in
+            self?.presentAlert(title: "More to add here", message: "Show game history is on the way!")
+        }
     }
 }
