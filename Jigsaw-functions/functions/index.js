5f8fed269bb348253c5ea561426ed27696a1f9a8
Ting Chen <tchen@esri.com>
2020-08-15 21:58:06 -0700

    Add 4 players queue.

--- Jigsaw-functions/functions/index.js
+++ Jigsaw-functions/functions/index.js
@@ -13,9 +13,55 @@ const db = admin.firestore();
 
 // Listens for new players added to /Queues/:documentId/twoPlayersQueue and creates an
 // game group to /GameGroups
-exports.makeGameGroup = functions.firestore.document('/Queues/Immigration/twoPlayersQueue/{userID}').onWrite(async (change, context) => {
+exports.makeTwoPlayersGameGroup = functions.firestore.document('/Queues/{category}/twoPlayersQueue/{userID}').onWrite(async (change, context) => {
   // Reference to the parent.
-  const ref = db.collection('/Queues/Immigration/twoPlayersQueue');
+  const ref = db.collection('/Queues/' + context.params.category + '/twoPlayersQueue');
+  
+  // Sort the players by their jigsawValue.
+  const jigsawValueQuery = await ref.orderBy('jigsawValue').get();
+  const playerIDs = [];
+  // create a map with all children that need to be removed
+  const updates = {};
+  jigsawValueQuery.forEach((doc) => {
+     playerIDs.push(doc.id)
+  })
+
+  const playersCount = playerIDs.length;
+  // Not enough players to create a game group.
+  if (playersCount === undefined || playersCount < 2) {
+    return null;
+  }
+
+  functions.logger.log('Players count = ', playersCount);
+  // Generate fields for a game group.
+  const group1 = [playerIDs[0]];  //, jigsawValueQuery[1]];
+  const group2 = [playerIDs[playersCount-1]];  // , jigsawValueQuery[playersCount-2]]
+  const createdDate = admin.firestore.FieldValue.serverTimestamp();
+  const chatroomID = "TestChatroom1";
+  const chatroomUserCount = 0;
+  const gameName = "USImmigration1";
+  
+  const gameGroup = {
+    "gameName": gameName,
+    "chatroomID": chatroomID,
+    "chatroomUserCount": chatroomUserCount,
+    "createdDate": createdDate,
+    "group1": group1,
+    "group2": group2
+  };
+  // Remove players added to a game group.
+  [].concat(group1, group2).forEach(async (id) => {
+      await ref.doc(id).delete();
+  })
+  // Add a new document in collection "GameGroups" with generated ID.
+  return db.collection('GameGroups').add(gameGroup);
+});
+
+// Listens for new players added to /Queues/:documentId/fourPlayersQueue and creates an
+// game group to /GameGroups
+exports.makeFourPlayersGameGroup = functions.firestore.document('/Queues/{category}/fourPlayersQueue/{userID}').onWrite(async (change, context) => {
+  // Reference to the parent.
+  const ref = db.collection('/Queues/' + context.params.category + '/fourPlayersQueue');
   
   // Sort the players by their jigsawValue.
   const jigsawValueQuery = await ref.orderBy('jigsawValue').get();
@@ -29,15 +75,15 @@ exports.makeGameGroup = functions.firestore.document('/Queues/Immigration/twoPla
 
   const playersCount = playerIDs.length;
   // Not enough players to create a game group.
-  if (playersCount === undefined || playersCount < 2) {
+  if (playersCount === undefined || playersCount < 4) {
     functions.logger.log('Not enough players to create game group.');
     return null;
   }
 
   functions.logger.log('Players count = ', playersCount);
   // Generate fields for a game group.
-  const group1 = [playerIDs[0]];  //, jigsawValueQuery[1]];
-  const group2 = [playerIDs[playersCount-1]];  // , jigsawValueQuery[playersCount-2]]
+  const group1 = [playerIDs[0], jigsawValueQuery[1]];
+  const group2 = [playerIDs[playersCount-2], jigsawValueQuery[playersCount-1]];
   const createdDate = admin.firestore.FieldValue.serverTimestamp();
   const chatroomID = "TestChatroom1";
   const chatroomUserCount = 0;
