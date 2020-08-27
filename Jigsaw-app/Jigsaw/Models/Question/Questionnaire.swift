915a648ab330850e251cdc3cad21f2303fe6c1a2
Ting Chen <tchen@esri.com>
2020-08-26 23:20:23 -0700

    Restructure project file.

--- Jigsaw-app/Jigsaw/Models/Question/Questionnaire.swift
+++ Jigsaw-app/Jigsaw/Models/Question/Questionnaire.swift
@@ -6,5 +6,8 @@
 //  Copyright © 2020 DukeMobileDevCenter. All rights reserved.
 //
 
-// Questionnaire is [Question], i.e. an array of questions.
+/// Questionnaire is [Question], i.e. an array of questions.
+/// - Note: Since we have 6 types of questions now, a questionnaire should not be
+///         an array of `Any`. Instead it is an array of objects that all conform to a
+///         protocol, which in this case is `QuestionEssentialProperty`.
 typealias Questionnaire = [QuestionEssentialProperty]
