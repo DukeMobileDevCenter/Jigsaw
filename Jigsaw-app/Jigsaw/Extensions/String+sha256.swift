c1858c3649e941bb9a2aa96da9eadb91eaa08c43
Ting Chen <tchen@esri.com>
2020-08-29 14:27:14 -0700

    Update hashing extension.

--- Jigsaw-app/Jigsaw/Extensions/String+sha256.swift
+++ Jigsaw-app/Jigsaw/Extensions/String+sha256.swift
@@ -7,16 +7,21 @@
 //
 
 import Foundation
-import CommonCrypto
+import CryptoKit
 
 extension String {
     var sha256: String {
         let data = Data(self.utf8)
-        var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
-        data.withUnsafeBytes {
-            _ = CC_SHA256($0.baseAddress, CC_LONG(data.count), &hash)
-        }
-        return hash.map { String(format: "%02x", $0) }.joined()
+        let digest = SHA256.hash(data: data)
+        return digest.map { String(format: "%02hhx", $0) }.joined()
+    }
+    
+    /// The md5 string of a data.
+    /// - Note: Refer to [CryptoKit](https://developer.apple.com/documentation/cryptokit).
+    var md5: String {
+        let data = Data(self.utf8)
+        let digest = Insecure.MD5.hash(data: data)
+        return digest.map { String(format: "%02hhx", $0) }.joined()
     }
     
     var gravatarURL: URL {
@@ -24,5 +29,11 @@ extension String {
         let endIndex = gravatarHash.index(gravatarHash.startIndex, offsetBy: 32)
         let gravatarURL = URL(string: "https://www.gravatar.com/avatar/\(gravatarHash[..<endIndex])?d=wavatar")!
         return gravatarURL
+    }
+    
+    /// The official implementation of Gravatar request.
+    /// - Note: Refer to [Gravatar Reference](http://en.gravatar.com/site/implement/images/).
+    var gravatarEmailURL: URL {
+        URL(string: "https://www.gravatar.com/avatar/\(self.md5)")!
     }
 }
