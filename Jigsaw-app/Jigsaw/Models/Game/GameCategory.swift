a0e57514471dc2ffccd1cb6fe5e0cee0c1b87eba
Ting Chen <tchen@esri.com>
2020-09-14 21:27:30 -0700

    Change game categories.

--- Jigsaw-app/Jigsaw/Models/Game/GameCategory.swift
+++ Jigsaw-app/Jigsaw/Models/Game/GameCategory.swift
@@ -12,25 +12,40 @@ import UIKit
 enum GameCategory: String, CaseIterable, Codable {
     // Some dummy categories only for demo purposes
     case immigration
-    case education
-    case housing
-    case medicare
-    case taxation
+    case economy
+    case law
+    case environment
+    case health
     
     var iconImage: UIImage {
         let iconImage: UIImage
         switch self {
         case .immigration:
             iconImage = UIImage(systemName: "hand.raised.slash")!
-        case .education:
-            iconImage = UIImage(systemName: "book")!
-        case .housing:
-            iconImage = UIImage(systemName: "house")!
-        case .medicare:
-            iconImage = UIImage(systemName: "staroflife")!
-        case .taxation:
+        case .economy:
             iconImage = UIImage(systemName: "dollarsign.circle")!
+        case .law:
+            iconImage = UIImage(systemName: "shield.lefthalf.fill")!
+        case .environment:
+            iconImage = UIImage(systemName: "cloud.sun")!
+        case .health:
+            iconImage = UIImage(systemName: "staroflife")!
         }
         return iconImage
+    }
+    
+    var label: String {
+        switch self {
+        case .immigration:
+            return "Immigration"
+        case .economy:
+            return "Economy"
+        case .law:
+            return "Law"
+        case .environment:
+            return "Environment"
+        case .health:
+            return "Health"
+        }
     }
 }
