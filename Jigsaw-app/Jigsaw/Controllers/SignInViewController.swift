1754009be62b616c8abcc60fb149b505cb9011a6
Ting Chen <tchen@esri.com>
2020-09-26 15:21:13 -0700

    Change VC xib to storyboard and modify files.

---
+++ Jigsaw-app/Jigsaw/Controllers/SignInViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignInViewController.swift
+//  Jigsaw
+//
+//  Created by Ting Chen on 9/26/20.
+//  Copyright © 2020 DukeMobileDevCenter. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+    
+    @available(*, unavailable)
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
