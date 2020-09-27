5053f78ba4e0972b0e69acee4d2085e4a5d657eb
Ting Chen <tchen@esri.com>
2020-09-27 16:33:10 -0700

    Add comments to game store.

--- Jigsaw-app/Jigsaw/Models/Game/GameStore.swift
+++ Jigsaw-app/Jigsaw/Models/Game/GameStore.swift
@@ -34,13 +34,13 @@ private class GameCollections {
 }
 
 class GameStore: NSObject {
-    // Singleton of the class.
+    /// Singleton of the `GameStore` class.
     static let shared = GameStore()
-    
+    /// A reference singleton to the `GameCollections` class.
     private let collections = GameCollections()
-    
+    /// An array that holds all games fetched from Firebase.
     var allGames = [Game]()
-    
+    /// The category which the GameStore should provide datasource.
     var selectedCategory: GameCategory!
     
     func getGames(for category: GameCategory) -> [Game] {
@@ -61,6 +61,10 @@ class GameStore: NSObject {
     }
     
     // swiftlint:disable cyclomatic_complexity
+    
+    /// Load all games from Firebase and save to respective categories.
+    ///
+    /// - Parameter completion: A closure that passes back an array of Games/rooms or an error.
     func loadGames(completion: @escaping (Result<[Game], Error>) -> Void) {
         var games = [Game]()
         // Clear all existing games.
@@ -99,6 +103,14 @@ class GameStore: NSObject {
         }
     }
     // swiftlint:enable cyclomatic_complexity
+    
+    /// Find next level room for current room/game.
+    ///
+    /// - Parameter currentGame: The room/game the player is currently in.
+    /// - Returns: The next level room if exists.
+    func nextGame(for currentGame: Game) -> Game? {
+        return allGames.first { $0.gameID == currentGame.nextLevelGameID }
+    }
 }
 
 extension GameStore: UICollectionViewDataSource {
