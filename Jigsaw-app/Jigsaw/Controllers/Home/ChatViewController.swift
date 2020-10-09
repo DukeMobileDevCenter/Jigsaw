ce32396a92d7538aeb52e2b94b17e927482b1acf
Ting Chen <tchen@esri.com>
2020-10-08 22:13:06 -0700

    Disable paste in chatroom.

--- Jigsaw-app/Jigsaw/Controllers/Home/ChatViewController.swift
+++ Jigsaw-app/Jigsaw/Controllers/Home/ChatViewController.swift
@@ -386,6 +386,13 @@ extension ChatViewController: InputBarAccessoryViewDelegate {
         // Clear the input field after sending the message.
         inputBar.inputTextView.text = ""
     }
+    
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        if action == #selector(UIResponderStandardEditActions.paste(_:)) {
+            return false
+        }
+        return super.canPerformAction(action, withSender: sender)
+    }
 }
 
 // MARK: - UIImagePickerControllerDelegate
